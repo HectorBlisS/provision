5f85771dbb0f64f1da2b9059c11125d5f023f8a8
BlisS <contacto@fixter.org>
2016-01-05 14:07:08 -0600

    permisos al panel admin

--- seguimiento/admin.py
+++ seguimiento/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import NuevaPregunta, Size, Plazo
+from django.contrib.auth.models import Permission
 
 admin.site.register(NuevaPregunta)
 admin.site.register(Size)
 admin.site.register(Plazo)
+admin.site.register(Permission)
 # Register your models here.
